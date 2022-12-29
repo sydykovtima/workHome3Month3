223c70afdb23e6dc83d95d5cd6bb2b61cdc1ec9a
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2022-12-29 19:17:06 +0600

    Update ViewController.swift

--- еуые/ViewController.swift
+++ еуые/ViewController.swift
@@ -8,35 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController  {
+  
+
+@IBOutlet weak var namesTableView: UITableView!
+    var names:[String] = ["Temirlan", "Dastan", "Kamila", "Aziza", "Gulkayir", "Bermet"]
+    var numbers:[String] = ["070324324", "099943234", "070373234","0504253234", "0777324324", "0999923423"]
     
-    @IBOutlet weak var Image: UITableViewCell!
-    @IBOutlet weak var dfaadf: UITableViewCell!
-    @IBOutlet weak var ZametkaTableView: UITableView!
-    var zametki:  [String ] = []
+ 
     
+    var addButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ZametkaTableView.register( UITableViewCell.self, forCellReuseIdentifier: "zametka_cell")
-        title = "Заметки"
-        navigationItem.rightBarButtonItem = UIBarButtonItem.init(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(rightBarButtonTap))
+  
+        namesTableView.delegate = self
+        name  sTableView.register(UITableViewCell.self, forCellReuseIdentifier: "name_cell")
+    }
+    
+    
+    
+   
+}
+extension ViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "name_cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+}
+extension ViewController:UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
         
     }
-    @objc func rightBarButtonTap() {
-        let alert = UIAlertController(title: "New zametrki", message: "", preferredStyle: .alert)
-        present(alert, animated: true, completion: nil)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let secondVc = storyboard?.instantiateViewController(withIdentifier: "second_vc") as! ViewController2
+        secondVc.info = names[indexPath.row] as! String
+        secondVc.num = numbers[indexPath.row] as! String
+        self.navigationController?.pushViewController(secondVc, animated: true)
     }
 }
-        extension ViewController: UITableViewDataSource {
-            func tableView(_ tableView: UITableView, numberOfRowsInSection sections: Int) -> Int{
-                return zametki.count
-            }
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "zametka_cell", for: indexPath)
-                cell.textLabel?.text = zametki[indexPath.row]
-                
-                return cell
-            }
-}
-
 
